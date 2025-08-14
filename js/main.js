d3467c23dfe8d4a97b4f49e56011fbd407938029
Leonid Seoshnik <leonidseoshnik@MacBook-Air-Leonid.local>
2025-08-14 17:23:05 +0300

    add two js functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const input = document.getElementById("playerNameInput");
 const button = document.getElementById("startButton");
+const screenRegister = document.querySelector(".screen-register");
 const screenHome = document.getElementById("screen-home");
 const playerNameDisplay = document.getElementById("playerNameDisplay");
 const startFightButton = document.getElementById("startFightButton");
+const goToCharacter = document.getElementById("goToCharacter");
+const screenCharacter = document.getElementById("screen-character");
 
 let player = {
   name: "",
@@ -16,7 +19,6 @@ button.addEventListener("click", () => {
   if (name) {
     player.name = name;
     console.log(`Имя сохранено: ${player.name}`);
-    const screenRegister = document.querySelector(".screen-register");
     screenRegister.style.display = "none";
     playerNameDisplay.textContent = player.name;
     screenHome.style.display = "block";
@@ -24,3 +26,16 @@ button.addEventListener("click", () => {
     alert("Пожалуйста, введите имя бойца.");
   }
 });
+
+goToCharacter.addEventListener("click", function () {
+  screenRegister.style.display = "none";
+  updateCharacterScreen();
+  screenCharacter.style.display = "block";
+});
+
+function updateCharacterScreen() {
+  document.getElementById("charPlayerName").textContent = player.name;
+  document.getElementById("characterAvatar").src = player.avatar;
+  document.getElementById("wins").textContent = player.wins;
+  document.getElementById("losses").textContent = player.losses;
+}
